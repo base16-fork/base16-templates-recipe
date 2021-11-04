26f3361ff54589a61cd41ba5f95b35fae6b71323
Khue Nguyen <Z5483Y@gmail.com>
2021-11-03 21:46:21 -0400

    Add script to fetch templates from original base16 sources

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+test -f ./builder/cbase16.cpp || git clone https://github.com/base16-fork/cbase16 ./builder >/dev/null
+make -C ./builder >/dev/null
+./builder/cbase16 update -l -c ./
+
+for d in $(find ./templates -maxdepth 2 -type d -name "templates"); do 
+	if [ ! -d "$(echo $d | cut -d '/' -f3)" ] && [ ! -z "$(echo $d | cut -d '/' -f3)" ]; then
+		mkdir "$(echo $d | cut -d '/' -f3)"
+	fi
+
+	cp -rf "$d" "$(dirname $d | sed "s/\.\/templates/\./")/" 2>/dev/null
+done
